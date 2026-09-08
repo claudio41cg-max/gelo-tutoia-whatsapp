722e3acc195601dc52de876c93669c7930081aff
claudio41cg-max <claudio41cg@gmail.com>
2026-09-08 16:47:13 -0300

    Refresh service worker for versioned PWA icon

--- public/app/sw.js
+++ public/app/sw.js
@@ -1,5 +1,5 @@
-const CACHE='gelo-tutoia-pwa-v73';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+const CACHE='gelo-tutoia-pwa-v75';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon-v75.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
