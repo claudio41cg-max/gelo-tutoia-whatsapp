b4717031a25d69749f44792e72b898e832a6ada1
claudio41cg-max <claudio41cg@gmail.com>
2026-09-08 17:32:56 -0300

    Backup parser before voice intelligence update

---
+++ parser.backup-v82.js
@@ -0,0 +1,3 @@
+// Backup do parser antes da atualização de inteligência de voz.
+// Blob original: 4d2fd1883c23a4def4b93bbde452c4bb9b97c35a
+// Para restaurar, recuperar parser.js no commit imediatamente anterior à atualização do parser.
